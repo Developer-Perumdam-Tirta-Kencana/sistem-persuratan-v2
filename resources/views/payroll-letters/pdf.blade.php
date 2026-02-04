8d2ff49b5a12064a9df4460cfa469116edb20839
asri_sobirin <asrisakbar123@gmail.com>
2026-02-04 10:45:20 +0700

    [feat] update surat pay rol template]

--- resources/views/payroll-letters/pdf.blade.php
+++ resources/views/payroll-letters/pdf.blade.php
@@ -15,10 +15,6 @@
             margin: 0;
             padding: 0;
         }
-        .kop-surat {
-            text-align: center;
-            margin-bottom: 0;
-        }
         .kop-surat img {
             width: 100%;
             display: block;
@@ -28,118 +24,121 @@
             border-bottom: 1px solid #000;
             margin: 10px 0 20px 0;
         }
-        .header {
-            text-align: center;
-            margin-bottom: 30px;
+        .content {
+            padding: 0 48px;
+            text-align: justify;
         }
-        .nomor-surat {
-            text-align: center;
+        .meta-table {
+            width: 100%;
             margin-bottom: 20px;
         }
-        .content {
-            text-align: justify;
-            margin-top: 10px;
-            padding: 0 48px;
-            box-sizing: border-box;
+        .meta-table td {
+            vertical-align: top;
+            padding: 2px 0;
         }
-        .table-data {
-            width: 100%;
-            margin: 20px 0;
-        }
-        .table-data td {
-            padding: 5px;
-        }
-        .ttd {
+        .ttd-area {
             margin-top: 50px;
-            text-align: right;
-        }
-        .ttd-space {
-            margin-top: 80px;
+            width: 100%;
         }
     </style>
 </head>
 <body>
-    @php
-        $kopBase64 = null;
-        if ($withKop) {
-            $kopPath = public_path('kop.png');
-            if (file_exists($kopPath)) {
-                $kopBase64 = 'data:image/png;base64,' . base64_encode(file_get_contents($kopPath));
-            }
-        }
-    @endphp
 
-    @if($kopBase64)
+@php
+    $kopBase64 = null;
+    if ($withKop) {
+        $kopPath = public_path('kop.png');
+        if (file_exists($kopPath)) {
+            $kopBase64 = 'data:image/png;base64,' . base64_encode(file_get_contents($kopPath));
+        }
+    }
+@endphp
+
+@if($kopBase64)
     <div class="kop-surat">
         <img src="{{ $kopBase64 }}" alt="Kop Surat">
     </div>
     <div class="garis-kop"></div>
-    @else
-    <div class="kop-surat" style="height: 120px;"></div>
-    @endif
+@endif
 
-    <div class="header">
-        <h3 style="margin: 5px 0;">SURAT PENGANTAR SLIP GAJI</h3>
-    </div>
+<div class="content">
 
-    <div class="nomor-surat">
-        <strong>Nomor: {{ $letter->nomor_surat }}</strong>
-    </div>
+    <p style="text-align:right;">
+        {{ \App\Models\SystemSetting::get('district', 'Jombang') }},
+        {{ $letter->tanggal_surat->translatedFormat('d F Y') }}
+    </p>
 
-    <div class="content">
-        <p>Kepada Yth,<br>
-        <strong>{{ strtoupper($letter->bank_tujuan) }}</strong><br>
-        Di Tempat</p>
+    <table class="meta-table">
+        <tr>
+            <td width="120">Nomor</td>
+            <td>: {{ $letter->nomor_surat }}</td>
+        </tr>
+        <tr>
+            <td>Sifat</td>
+            <td>: Penting</td>
+        </tr>
+        <tr>
+            <td>Lampiran</td>
+            <td>: 2 (dua) lembar</td>
+        </tr>
+        <tr>
+            <td>Hal</td>
+            <td>: Permohonan Pemindahbukuan Rekening</td>
+        </tr>
+    </table>
 
-        <p>Dengan hormat,</p>
+    <p>
+        Kepada Yth.<br>
+        <strong>Pimpinan Bank Jatim</strong><br>
+        Kantor Cabang {{ $letter->bank_cabang ?? 'Jombang' }}<br>
+        Di –<br>
+        <strong>{{ strtoupper($letter->bank_cabang ?? 'JOMBANG') }}</strong>
+    </p>
 
-        <p style="text-indent: 30px;">
-            Bersama ini kami sampaikan slip gaji karyawan untuk periode <strong>{{ $letter->bulan_gaji }}</strong> 
-            dengan total nominal sebesar <strong>Rp {{ number_format($letter->total_nominal, 0, ',', '.') }}</strong> 
-            ({{ ucwords(terbilang($letter->total_nominal)) }} Rupiah).
-        </p>
+    <p>Dengan hormat,</p>
 
-        <table class="table-data">
-            <tr>
-                <td width="200">Tanggal</td>
-                <td>: {{ $letter->tanggal_surat->format('d F Y') }}</td>
-            </tr>
-            <tr>
-                <td>Bulan Gaji</td>
-                <td>: {{ $letter->bulan_gaji }}</td>
-            </tr>
-            <tr>
-                <td>Bank Tujuan</td>
-                <td>: {{ $letter->bank_tujuan }}</td>
-            </tr>
-            <tr>
-                <td>No. Rekening Sumber</td>
-                <td>: {{ $letter->nomor_rekening_sumber }}</td>
-            </tr>
-            <tr>
-                <td>Total Nominal</td>
-                <td>: Rp {{ number_format($letter->total_nominal, 0, ',', '.') }}</td>
-            </tr>
-        </table>
+    <p style="text-indent:30px;">
+        Sehubungan dengan pemberian gaji bulan
+        <strong>{{ $letter->bulan_gaji }}</strong>
+        bagi Pegawai
+        <strong>{{ \App\Models\SystemSetting::get('company_line2', 'PERUMDAM TIRTA KENCANA KABUPATEN JOMBANG') }}</strong>
+        yang dibayarkan melalui rekening Bank Jatim Cabang Jombang,
+        maka bersama ini kami mohon kepada Bank Jatim Cabang Jombang
+        untuk <strong>MEMINDAHBUKUKAN</strong> rekening Simpeda atas nama
+        <strong>{{ \App\Models\SystemSetting::get('company_line2') }}</strong>
+        dengan nomor rekening
+        <strong>{{ $letter->nomor_rekening_sumber }}</strong>
+        sebesar
+        <strong>
+            Rp {{ number_format($letter->total_nominal, 0, ',', '.') }}
+        </strong>
+        ({{ ucwords(terbilang($letter->total_nominal)) }} Rupiah)
+        ke rekening masing-masing pegawai sesuai data terlampir.
+    </p>
 
-        <p>Demikian surat pengantar ini kami sampaikan. Atas perhatian dan kerjasamanya kami ucapkan terima kasih.</p>
-    </div>
+    <p>
+        Demikian mohon perhatiannya dan atas segala informasi serta
+        kerjasamanya kami sampaikan terima kasih.
+    </p>
 
-    <div class="ttd">
-        <p>{{ \App\Models\SystemSetting::get('district', 'Kabupaten Jombang') }}, {{ $letter->tanggal_surat->format('d F Y') }}</p>
-        <p><strong>Hormat kami,</strong></p>
-        <p><strong>{{ \App\Models\SystemSetting::get('company_line1', 'Perusahaan Umum Daerah Air Minum') }}</strong></p>
-        <p><strong>{{ \App\Models\SystemSetting::get('company_line2', 'Tirta Kencana Jombang') }}</strong></p>
-        <div class="ttd-space"></div>
-        <p><strong>{{ \App\Models\SystemSetting::get('director_name', 'KHOIRUL HASYIM. S.Pd, M.Pd') }}</strong></p>
-        <p>NIP. {{ \App\Models\SystemSetting::get('director_nip', '19800815 202502 1 001') }}</p>
-        @if($letter->status === 'disetujui' && $letter->approver)
-        <hr style="margin-top: 40px; margin-bottom: 20px;">
-        <p style="font-size: 10pt; color: #666;">
-            Disetujui oleh: {{ $letter->approver->name }} <br>
-            Tanggal: {{ $letter->approved_at ? $letter->approved_at->translatedFormat('d F Y H:i') : '' }}
-        </p>
-        @endif
-    </div>
+    <table class="ttd-area">
+        <tr>
+            <td width="50%" style="text-align:center;">
+                Mengetahui,<br>
+                <strong>Direktur</strong><br>
+                {{ \App\Models\SystemSetting::get('company_line2') }}<br><br><br><br>
+                <strong>{{ \App\Models\SystemSetting::get('director_name') }}</strong><br>
+                NIP. {{ \App\Models\SystemSetting::get('director_nip') }}
+            </td>
+            <td width="50%" style="text-align:center;">
+                Manajer Administrasi & Keuangan<br><br><br><br><br>
+                <strong>{{ \App\Models\SystemSetting::get('finance_manager_name', 'NORMA DIARINI, S.Sos') }}</strong><br>
+                NIP. {{ \App\Models\SystemSetting::get('finance_manager_nip', '19731228 200012 2 041') }}
+            </td>
+        </tr>
+    </table>
+
+</div>
+
 </body>
 </html>
